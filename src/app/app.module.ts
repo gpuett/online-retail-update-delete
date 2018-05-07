8a3e4f1c562b6ea1d2f1b260ee8642858f55cd17
Brenda Franco and Ted Leary <brenda@>
2018-05-07 10:43:49 -0700

    entire homework assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+
+import { AboutComponent } from './about/about.component';
+import { routing } from './app.routing';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    MarketplaceComponent,
+    AlbumDetailComponent,
+    AboutComponent,
+    AlbumDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  //   FormsModule,
+  //  HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
