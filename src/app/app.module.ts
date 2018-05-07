eca59f8af6002c22487ad64da3ed2a20c3495835
Brenda Franco and Ted Leary <brenda@>
2018-05-07 11:58:53 -0700

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { AboutComponent } from './about/about.component';
 import { routing } from './app.routing';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 
 @NgModule({
@@ -24,7 +35,9 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
     BrowserModule,
   //   FormsModule,
   //  HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+   AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
